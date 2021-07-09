c0bd6cfdf0b3d1e0d598e5047ec82c195bf0612a
corneliusmeyer <cornelius.meyer333@gmail.com>
2021-07-10 00:15:11 +0200

    erste Widgets hinzugefügt

--- formexclusiv_inventur/lib/screens/homescreen.dart
+++ formexclusiv_inventur/lib/screens/homescreen.dart
@@ -10,9 +10,35 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    bool hasData = true;
     return Scaffold(
       appBar: AppBar(),
-      body: null,
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/fe_logo.png",
+              height: 130,
+            ),
+            Text("Inventur"),
+            (hasData == true)
+                ? ElevatedButton.icon(
+                    onPressed: null,
+                    icon: Icon(Icons.mode_edit),
+                    label: Text('Inventur fortsetzen'))
+                /*ElevatedButton.icon(
+                        onPressed: null,
+                        icon: Icon(Icons.ac_unit_outlined),
+                        label: Text('Inventur abschließen')),
+                    ElevatedButton.icon(
+                        onPressed: null,
+                        icon: Icon(Icons.ac_unit_outlined),
+                        label: Text('Inventur löschen'))*/
+
+                : Text('Neue Inventur anlegen'),
+          ],
+        ),
+      ),
     );
   }
 }
